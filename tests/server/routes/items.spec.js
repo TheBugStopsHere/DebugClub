02e124bc64f0ebeabe2aff3d5a31e1eee64db81e
Carolyn <ccpianist@gmail.com>
2019-01-09 10:34:24 -0800

    first GET route working & cleaned up file structure

--- tests/server/routes/items.spec.js
+++ tests/server/routes/items.spec.js
@@ -7,37 +7,20 @@ const db = require('../../../server/db')
 const app = require('../../../server/index')
 const Item = db.model('item')
 
-describe('Item routes', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
+describe('Item api routes', () => {
+  const fakeClient = supertest(app)
+
+  beforeEach(() => db.sync({force: true}))
+  beforeEach(async () => {
+    await Item.create({
+      name: 'Riley',
+      price: 1000000
+    })
   })
 
-  describe('/api/items/', () => {
-    const fakeClient = supertest(app)
-    const rileysEmail = 'roo@doggo.com'
-
-    beforeEach(async () => {
-      await Item.create({
-        name: 'Riley',
-        email: rileysEmail,
-        price: 1000000
-      })
-    })
-
-    // it('GET /api/items', async () => {
-    //   const res = await request(app)
-    //     .get('/api/items')
-    //     .expect(200)
-
-    //   expect(res.body).to.be.an('array')
-    //   expect(res.body[0].email).to.be.equal(rileysEmail)
-    // })
-
-    it('GET /api/items', async () => {
-      const res = await fakeClient.get('/api/items')
-      console.log(res)
-      // expect(res.body).to.be.an('array')
-      // expect(res.body[0].email).to.be.equal(rileysEmail)
-    })
-  }) // end describe('/api/items')
+  it('gets all items', async () => {
+    const res = await fakeClient.get('/api/items')
+    expect(res.body).to.be.an('array')
+    expect(res.body[0].name).to.be.equal('Riley')
+  }) // end describe('gets all items')
 }) // end describe('Item routes')
