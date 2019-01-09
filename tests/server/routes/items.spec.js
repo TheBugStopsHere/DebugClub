8c6824453d4694ae2d30ce719a2538a8ff9320d8
Carolyn <ccpianist@gmail.com>
2019-01-09 11:50:32 -0800

    non-admin api routes finished

--- tests/server/routes/items.spec.js
+++ tests/server/routes/items.spec.js
@@ -5,6 +5,7 @@ const request = require('supertest')
 const supertest = require('supertest')
 const db = require('../../../server/db')
 const app = require('../../../server/index')
+// const agent = supertest.agent(app)
 const Item = db.model('item')
 
 describe('Item api routes', () => {
@@ -29,4 +30,15 @@ describe('Item api routes', () => {
     const res = await fakeClient.get(`/api/items/${riley.dataValues.id}`)
     expect(res.body.id).to.be.equal(1)
   }) // end describe('gets a single item')
+
+  // ADMIN routes TBD:
+  // post/items
+
+  // it('creates an item', async () => {
+  //   const res = await fakeClient.post(`/api/items`).send(riley)
+  //   expect(res.body.id).to.be.equal(1)
+  // }) // end describe('creates an item')
+
+  // put/items/:itemId
+  // del/items/:itemId
 }) // end describe('Item routes')
