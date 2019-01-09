b2dbfc78065e058068b349d26ebbb45bc5510336
Carolyn <ccpianist@gmail.com>
2019-01-08 16:14:04 -0800

    specs working for Item column definitions

--- tests/server/models/item.spec.js
+++ tests/server/models/item.spec.js
@@ -7,9 +7,10 @@ const Item = db.model('item')
 describe('Item model', () => {
   beforeEach(() => db.sync({force: true}))
 
-  describe('column definitions and validations', () => {
-    it('has keys for `name`, `imageURL`, `description`, and `category`', async () => {
-      const bumblebee = await Item.create({
+  describe('column definitions', () => {
+    let bumblebee
+    beforeEach(async () => {
+      bumblebee = await Item.create({
         name: 'Bumblebee',
         imageURL:
           'https://irp-cdn.multiscreensite.com/ed883b94/dms3rep/multi/mobile/a53e985a43c4489dabf6c38d196501e9-608x681.dm.edit_rRHlBn.jpg',
@@ -18,15 +19,29 @@ describe('Item model', () => {
           'A bumblebee (or bumble bee, bumble-bee or humble-bee) is any of over 250 species in the genus Bombus, part of Apidae, one of the bee families.',
         category: 'live bugs'
       })
+    })
 
+    it('has correct `name` property', async () => {
       expect(bumblebee.name).to.equal('Bumblebee')
+    })
+
+    it('has correct `imageURL` property', async () => {
       expect(bumblebee.imageURL).to.equal(
         'https://irp-cdn.multiscreensite.com/ed883b94/dms3rep/multi/mobile/a53e985a43c4489dabf6c38d196501e9-608x681.dm.edit_rRHlBn.jpg'
       )
+    })
+
+    it('has correct `price` property', async () => {
       expect(bumblebee.price).to.equal(parseInt(3.5).toFixed(2))
+    })
+
+    it('has correct `description` property', async () => {
       expect(bumblebee.description).to.equal(
         'A bumblebee (or bumble bee, bumble-bee or humble-bee) is any of over 250 species in the genus Bombus, part of Apidae, one of the bee families.'
       )
+    })
+
+    it('has correct `category` property', async () => {
       expect(bumblebee.category).to.equal('live bugs')
     })
 
