4e4b1e4968332a7dcd734f5f3ea58ec08e1259a1
Jacob Rosen <jacob360@gmail.com>
2019-01-10 11:48:21 -0800

    feature(checkout) checkout form with payment component

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, Checkout} from './components'
 import app from './app'
 import {me} from './store'
 
@@ -23,6 +23,7 @@ class Routes extends Component {
         <Route exact path="/" component={app} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/signup" component={Signup} />
+        <Route exact path="/checkout" component={Checkout} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
