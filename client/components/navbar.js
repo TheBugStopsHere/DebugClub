27315f284fb93ce1cdd9b72a7958fd225bbb73af
Carolyn <ccpianist@gmail.com>
2019-01-10 15:45:42 -0800

    bugfix(login): move signup into login and reformat navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,36 +1,42 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
-  <nav className="navbar navbar-inverse navbar-default navbar-fixed-top">
-    <Link to="/">
+  <nav
+    className="navbar navbar-inverse navbar-default navbar-fixed-top"
+    id="navItems"
+  >
+    <Link to="/" id="linkHome">
       <img id="logo" src="/bugRed.png" />
       <h1 id="title">The Bug Stops Here</h1>
     </Link>
-    <div id="navItems">
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">My Profile</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-          <Link to="/cart">My Cart</Link>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/cart">My Cart</Link>
-        </div>
-      )}
-    </div>
+    {isLoggedIn ? (
+      <Fragment>
+        {/* The navbar will show these links after you log in */}
+        <Link to="/home" className="navItem">
+          My Profile
+        </Link>
+        <a href="#" onClick={handleClick}>
+          Logout
+        </a>
+        <Link to="/cart" className="navItem">
+          My Cart
+        </Link>
+      </Fragment>
+    ) : (
+      <Fragment>
+        {/* The navbar will show these links before you log in */}
+        <Link to="/login" className="navItem">
+          Login
+        </Link>
+        <Link to="/cart" className="navItem">
+          My Cart
+        </Link>
+      </Fragment>
+    )}
   </nav>
 )
 
