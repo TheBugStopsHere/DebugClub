4a5c4bb2525b312bc6dd0c9cc926933acb62bdcf
Colleen Risso <colleenrisso@gmail.com>
2019-01-15 11:56:40 -0800

    feature(test) add test for single item component

---
+++ tests/client/Single-Items.spec.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai'
+import enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import React from 'react';
+import {SingleItem} from '../../client/components/Single-Item';
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('SingleItem', () => {
+      let singleItem
+
+      const item = { name: 'Grasshopper'} ;
+    
+      beforeEach(() => {
+        singleItem = shallow(<SingleItem item={item} />)
+      })
+
+    it('renders the name in an h1', () => {
+        expect(singleItem.find('h1').text()).to.be.equal('Grasshopper')
+    })
+})
