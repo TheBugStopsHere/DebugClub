36f6f77f976f1fc83384732a294bde5dd881c264
Carolyn <ccpianist@gmail.com>
2019-01-14 21:03:48 -0800

    bugfix(orderConfirmation): close confirmation redirects home

--- client/components/Order-Confirmation.js
+++ client/components/Order-Confirmation.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class OrderConfirmation extends Component {
   constructor() {
@@ -15,24 +16,29 @@ class OrderConfirmation extends Component {
   }
 
   render() {
-    return  (
+    return (
       <div id="confirmation" className="modal-dialog">
         <div className="modal-content">
           <div className="modal-header">
-            <button type="button" className="close" data-dismiss="modal">
-              &times;
-            </button>
+            <Link to="/">
+              <button type="button" className="close" data-dismiss="modal">
+                &times;
+              </button>
+            </Link>
             <h1>Order Confirmation</h1>
           </div>
           <div className="modal-body">
-            <h2>Thank you, {this.props.name || 'fellow bug lover'}! Your order is on its way!</h2>
+            <h2>
+              Thank you, {this.props.name || 'fellow bug lover'}! Your order is
+              on its way!
+            </h2>
             <h2>
               Order #{this.randomNum()}-{this.props.orderNum}
             </h2>
           </div>
         </div>
       </div>
-    ) 
+    )
   }
 }
 
--- client/components/Payment.js
+++ client/components/Payment.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import axios from 'axios'
 import {CardElement, injectStripe} from 'react-stripe-elements'
-import OrderConfirmation from './Order-Confirmation'
 
 class Payment extends React.Component {
   constructor(props) {
     super(props)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.modalBackdrop = this.modalBackdrop.bind(this)
-    this.state = {
-      complete: false
-    }
   }
 
   async handleSubmit() {
@@ -31,30 +26,11 @@ class Payment extends React.Component {
     if (response.data.status === 'succeeded') {
       console.log('Purchase Complete!')
       this.props.handleShippingSubmit()
-      this.setState({complete: true})
-      // this.modalBackdrop()
-      // $('#confirmation').modal({backdrop: 'static'})
     }
   }
 
-  modalBackdrop() {
-    $('#submit').click(function() {
-      $('#confirmation').modal('show')
-    })
-  }
-
   render() {
-    return this.state.complete ? (
-      <div>
-        <OrderConfirmation
-          name={this.props.name}
-          orderNum={this.props.id}
-          id="confirmation"
-          className="modal fade"
-          role="dialog"
-        />
-      </div>
-    ) : (
+    return (
       <div className="payment">
         <CardElement />
         <button
